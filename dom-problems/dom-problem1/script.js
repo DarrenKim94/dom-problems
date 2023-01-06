745c6c6a06fb61ea5fe2889bb53c4cb0f1a7bf15
Darren Kim <dkim555@yahoo.com>
2023-01-05 21:25:15 -0800

    add onclock event in html

--- dom-problems/dom-problem1/script.js
+++ dom-problems/dom-problem1/script.js
@@ -1,1 +1,10 @@
-document.getElementsByTagName('p')
+// Create a basic HTML page with a P element and a button on it.
+
+// Then, write a JavaScript function to set the background color of that paragraph using an "onclick" event added to a button in the UI.
+
+document.getElementById("btn").onclick = function() {changeColor()};
+
+function changeColor() {
+  document.getElementById("btn").style.backgroundColor = "red";
+}
+   
