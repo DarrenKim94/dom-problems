ed14899e067cc883054106709efd5ab41c75d735
Darren Kim <dkim555@yahoo.com>
2023-01-10 14:17:14 -0800

    add for loop

--- dom-problems/dom-problem3/script.js
+++ dom-problems/dom-problem3/script.js
@@ -1,1 +1,14 @@
-const btn = document.getElementById('click')
+
+
+function showColors() {
+    const amount = document.getElementById('select').options.length;
+    document.getElementById('p1').innerHTML = 'There are ' + amount + ' colors';
+   
+
+    const color = document.getElementById('select');
+    let txt = '';
+    for(let i = 0; i < color.length; i++) {
+        txt = txt + color.options[i].text + ', ';
+    }
+    document.getElementById('p2').innerHTML = txt;
+}
